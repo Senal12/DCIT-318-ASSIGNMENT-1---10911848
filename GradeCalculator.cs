9c2dc4b058ee39928e362904ae1a3d3e0979f209
ALBERT ASARE BAFFOUR <senalboy11@gmail.com>
2024-06-20 21:33:37 +0100

    Add files via upload

---
+++ GradeCalculator.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeCalculator();
+        }
+
+        static void GradeCalculator()
+        {
+            Console.WriteLine("Enter a numerical grade between 0 and 100:");
+            int grade = int.Parse(Console.ReadLine());
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("Grade: A");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine("Grade: B");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("Grade: C");
+            }
+            else if (grade >= 60)
+            {
+                Console.WriteLine("Grade: D");
+            }
+            else
+            {
+                Console.WriteLine("Grade: F");
+            }
+        }
+    }
+}
